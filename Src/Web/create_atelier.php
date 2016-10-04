088212c3d2194ccc2946a9289973bad80f44cfb6
ismael TRAORE <ismo@MacBook-Pro-de-ismael.local>
2016-10-04 20:58:46 +0200

    modificication create_atelier.php

--- Src/Web/create_atelier.php
+++ Src/Web/create_atelier.php
@@ -1,3 +1,25 @@
+<?php
+
+	
+ if (isset($_POST["validate"]))
+ {
+ 	
+   	 $title=$_POST['title'];
+   	 $theme=$_POST['theme'];
+   	 $type=$_POST['type'];
+   	 $place=$_POST['place'];
+   	 $duration=$_POST['duration'];
+   	 $capacity=$_POST['capacity'];
+   	 $id_labo = 3;
+   	 $id_creneaux = 2;
+   	 $id_atelier= 4;   	
+   	 $remarque = "Remarque";
+   	 $query = "INSERT INTO atelier (id_Atelier,titre, theme, type, Remarque, lieu, duree, capacite,id_creneaux, id_labo) VALUES (_id_atelier,_title, _theme, _type, _remarque, _place, _duration, _capacity,_id_creneaux,_id_lab)"; 
+   	 $request = $bdd->prepare($query);
+   	 $request->execute(array("_id_atelier" => $id_atelier, "_title"=>$titre, "_theme"=>$theme, "_type"=>$type, "_remarque" => $remarque, "_place"=>$lieu, "_duration"=>$duree, "_capacity"=>$capacite, "_id_creneaux"=>$id_creneaux,"_id_lab"=>$id_lab));
+}
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -9,11 +31,11 @@
 <body>
 <form action="create_atelier.php">
   <h1>Ajout d'un atelier</h1>
-  <input type="text" name="firstname" value="Titre">
+  <input type="text" name="title" value="Titre">
   <br>
-  <input type="text" name="lastname" value="Thème">
+  <input type="text" name="theme" value="Thème">
   <br>
-  <input type="text" name="firstname" value="Type">
+  <input type="text" name="type" value="Type">
   <br>
 	Date : <br>
   <TABLE>
@@ -59,20 +81,19 @@
     </tr>
     
   </TABLE>
-  <input type="text" name="lastname" value="Laboratoire">
+  <input type="text" name="labo" value="Laboratoire">
   <br>
-  <input type="text" name="lastname" value="Lieu">
+  <input type="text" name="place" value="Lieu">
   <br>
   <textarea rows="4" cols="50">
    Remarque
   </textarea>
   <br>
-  <input type="text" name="lastname" value="Dureé de l'atelier">
+  <input type="text" name="duration" value="Durée de l'atelier">
   <br>
-  <input type="text" name="lastname" value="Capacité">
+  <input type="text" name="capacity" value="Capacité">
   <br>
-  
-  <input type="submit" value="valider">
+  <input type="submit" name = "validate" value="valider">
 </form> 
 </body>
 </html>
