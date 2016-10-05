1989eb3b433300b371ea55e84bd942c7bc0d0d3a
Shqrk <thomaslabrousse@orange.fr>
2016-10-05 20:35:07 +0200

    Retour liste delete atelier

--- Src/Web/delete_atelier.php
+++ Src/Web/delete_atelier.php
@@ -15,9 +15,18 @@ if(isset($_GET['supp_article'])){
 <html>
 <head>
 <title>Fête de la science</title>
+<link rel="stylesheet" type="text/css" href="css/style_form.css">
  <meta charset="UTF-8"> 
 </head>
 <body>
 <h1> Votre atelier a bien été supprimer</h1>
+
+
+<form method="POST" action="list_atelier.php">
+<input type="submit" name = "validate" value="Retour liste" >
+</form>
+
+
 </body>
 </html>
+
