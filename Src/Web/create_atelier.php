9c09a978297b8e02aa3d3ff991417d3d7f752fb9
ismael TRAORE <ismo@MacBook-Pro-de-ismael.local>
2016-10-05 09:29:13 +0200

    implémentation partielle de la création atelier

--- Src/Web/create_atelier.php
+++ Src/Web/create_atelier.php
@@ -1,6 +1,46 @@
 <?php
 
+	include("connexion.php");
 	
+	/**
+	 * Create a new workshop.
+	 */
+	 
+	function create_workshop($bdd, $arguments)
+	{
+		$state = array();
+
+		$keys = array_keys($arguments);
+		$values = array_values($arguments);
+
+		$sql_query = $bdd->prepare("INSERT INTO " . "atelier" . " (titre , theme, type, Remarque, lieu, duree, capacite,id_creneaux, id_labo)"
+												." VALUES ( :title, :theme, :type, :remarque, :place, :duration, :capacity, :id_creneaux, :id_lab)");
+		
+		$sql_query->bindParam($keys[0], $values[0]);
+		$sql_query->bindParam($keys[1], $values[1]);
+		$sql_query->bindParam($keys[2], $values[2]);
+		$sql_query->bindParam($keys[3], $values[3]);
+		$sql_query->bindParam($keys[4], $values[4]);
+		$sql_query->bindParam($keys[5], $values[5], PDO::PARAM_INT);
+		$sql_query->bindParam($keys[6], $values[6], PDO::PARAM_INT);
+		$sql_query->bindParam($keys[7], $values[7], PDO::PARAM_INT);
+		$sql_query->bindParam($keys[8], $values[8], PDO::PARAM_INT);
+
+
+		$sql_query->execute();
+		
+		echo $values[0];
+		
+		if ($sql_query) {
+			$state['query'] = 1;
+		}
+		else {
+			$state['query'] = 0;
+		}
+		
+		return $state;
+ 	}	
+ 	
  if (isset($_POST["validate"]))
  {
  	
@@ -10,13 +50,13 @@
    	 $place=$_POST['place'];
    	 $duration=$_POST['duration'];
    	 $capacity=$_POST['capacity'];
-   	 $id_labo = 3;
-   	 $id_creneaux = 2;
-   	 $id_atelier= 4;   	
+   	 $id_labo = '1';
+   	 $id_creneaux = '1';
+   	 $id_atelier = '4';
    	 $remarque = "Remarque";
-   	 $query = "INSERT INTO atelier (id_Atelier,titre, theme, type, Remarque, lieu, duree, capacite,id_creneaux, id_labo) VALUES (_id_atelier,_title, _theme, _type, _remarque, _place, _duration, _capacity,_id_creneaux,_id_lab)"; 
-   	 $request = $bdd->prepare($query);
-   	 $request->execute(array("_id_atelier" => $id_atelier, "_title"=>$titre, "_theme"=>$theme, "_type"=>$type, "_remarque" => $remarque, "_place"=>$lieu, "_duration"=>$duree, "_capacity"=>$capacite, "_id_creneaux"=>$id_creneaux,"_id_lab"=>$id_lab));
+   	 $workshop_data = array(":title" => $title , ":theme"=>$theme, ":type"=>$type, ":remarque" => $remarque, ":place"=>$place, ":duration"=>$duration, ":capacity"=>$capacity, ":id_creneaux"=>$id_creneaux,":id_lab"=>$id_labo);
+	 $response = create_workshop($bdd,$workshop_data);
+
 }
 ?>
 
@@ -29,13 +69,13 @@
  <meta charset="UTF-8"> 
 </head>
 <body>
-<form action="create_atelier.php">
+<form method="POST" >
   <h1>Ajout d'un atelier</h1>
-  <input type="text" name="title" value="Titre">
+  <input type="text" name="title" >
   <br>
-  <input type="text" name="theme" value="Thème">
+  <input type="text" name="theme"  >
   <br>
-  <input type="text" name="type" value="Type">
+  <input type="text" name="type"  >
   <br>
 	Date : <br>
   <TABLE>
@@ -81,17 +121,33 @@
     </tr>
     
   </TABLE>
-  <input type="text" name="labo" value="Laboratoire">
+      <select name="labo">
+          <?php
+          $sql_lab = $bdd->prepare('SELECT * FROM laboratoire');
+          $sql_lab->execute();
+          if ( $sql_lab->rowCount() > 0 ) {
+            $sql_lab->setFetchMode(PDO::FETCH_ASSOC);
+
+              while ($lab = $sql_lab->fetch()) { ?>
+
+
+                  <option value = "<?php echo $lab['nom']?>"> "<?php echo $lab['nom']?>" </option>
+
+
+              <?php }}
+          $sql_lab->closeCursor(); // we will not use it anymore, so we must close server connection
+          ?>
+      </select>
   <br>
-  <input type="text" name="place" value="Lieu">
+  <input type="text" name="place">
   <br>
   <textarea rows="4" cols="50">
    Remarque
   </textarea>
   <br>
-  <input type="text" name="duration" value="Durée de l'atelier">
+  <input type="text" name="duration" >
   <br>
-  <input type="text" name="capacity" value="Capacité">
+  <input type="text" name="capacity" >
   <br>
   <input type="submit" name = "validate" value="valider">
 </form> 
