695786ccf30b6521dbf5894b34e4e65ce39a2b00
Shqrk <thomaslabrousse@orange.fr>
2016-10-05 20:28:02 +0200

    Suppression dans la liste

--- Src/Web/delete_atelier.php
+++ Src/Web/delete_atelier.php
@@ -1,3 +1,16 @@
+<?php 
+include("connexion.php");
+if(isset($_GET['supp_article'])){
+  
+  $rep = $bdd->prepare('DELETE FROM atelier WHERE id_Atelier =?');
+  $rep->execute(array($_GET['supp_article']));
+  $donnees = $rep->fetch();
+  $rep->closeCursor();
+  }
+?>
+
+
+
 <!DOCTYPE html>
 <html>
 <head>
