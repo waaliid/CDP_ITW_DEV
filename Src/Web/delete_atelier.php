b97a77ecdf02352eb7b1b1826db2f8b8a9a37617
waliid <walid.mamouni@gmail.com>
2016-10-06 23:29:25 +0200

    [Src/Web] Ajout de la suppression du creneau correspondant et une redirection vers la liste

--- Src/Web/delete_atelier.php
+++ Src/Web/delete_atelier.php
@@ -5,28 +5,14 @@ if(isset($_GET['supp_article'])){
   $rep = $bdd->prepare('DELETE FROM atelier WHERE id_Atelier =?');
   $rep->execute(array($_GET['supp_article']));
   $donnees = $rep->fetch();
+
+  $rep2 = $bdd->prepare('DELETE FROM creneaux WHERE id_atelier =?');
+  $rep2->execute(array($_GET['supp_article']));
+  $donnees2 = $rep2->fetch();
+  
   $rep->closeCursor();
+  $rep2->closeCursor();
+
+  header('Location: list_atelier.php');
   }
 ?>
-
-
-
-<!DOCTYPE html>
-<html>
-<head>
-<title>Fête de la science</title>
-<link rel="stylesheet" type="text/css" href="css/style_form.css">
- <meta charset="UTF-8"> 
-</head>
-<body>
-<h1> Votre atelier a bien été supprimer</h1>
-
-
-<form method="POST" action="list_atelier.php">
-<input type="submit" name = "validate" value="Retour liste" >
-</form>
-
-
-</body>
-</html>
-
