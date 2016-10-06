447df1752f8db69eca1b021e86059efb5317fe9b
waliid <walid.mamouni@gmail.com>
2016-10-06 17:38:29 +0200

    [Src/Web] Mise à jour de la pagination

--- Src/Web/list_atelier.php
+++ Src/Web/list_atelier.php
@@ -32,11 +32,14 @@
 $page = (!empty($_GET['page']) ? $_GET['page'] : 1);
 $limite = 5;
 $debut = ($page - 1) * $limite;
-$query = 'SELECT * FROM atelier LIMIT :limite OFFSET :debut';
+$query = 'SELECT SQL_CALC_FOUND_ROWS * FROM atelier LIMIT :limite OFFSET :debut';
 $query = $bdd->prepare($query);
 $query->bindValue('limite', $limite, PDO::PARAM_INT);
 $query->bindValue('debut', $debut, PDO::PARAM_INT);
 $query->execute();
+
+$resultFoundRows = $bdd->query('SELECT found_rows()');
+$nombredElementsTotal = $resultFoundRows->fetchColumn();
 
 while ($donnees = $query->fetch())
 { ?>
@@ -62,9 +65,18 @@ while ($donnees = $query->fetch())
 <?php }
 ?>
 </table>
-<a href="?page=<?php echo $page - 1; ?>">Page précédente</a>
-—
-<a href="?page=<?php echo $page + 1; ?>">Page suivante</a>
+<?php
+$nombreDePages = ceil($nombredElementsTotal / $limite);
+if ($page > 1):
+    ?><a href="?page=<?php echo $page - 1; ?>">Page précédente</a> — <?php
+endif;
+for ($i = 1; $i <= $nombreDePages; $i++):
+    ?><a href="?page=<?php echo $i; ?>"><?php echo $i; ?></a> <?php
+endfor;
+if ($page < $nombreDePages):
+    ?> — <a href="?page=<?php echo $page + 1; ?>">Page suivante</a><?php
+endif;
+?>
 </div>
 
 <div class= "form-inline">
